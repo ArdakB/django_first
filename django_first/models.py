98ee52752063d2c8490cb0e6ca67e2017b4f6a4c
U-NSK\Bakirova_A <Bakirova_A@PC017.nsk.kz>
2019-04-14 21:36:19 +0600

    Add order process

--- django_first/models.py
+++ django_first/models.py
@@ -3,29 +3,68 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
+    price = models.DecimalField(
+        max_digits=10, decimal_places=2,
+        null=True, blank=True
+    )
 
 
 class Store(models.Model):
+    location = models.CharField(max_length=100)
+
+
+class StoreItem(models.Model):
+    store = models.ForeignKey(
+        Store,
+        on_delete=models.CASCADE,
+        related_name='store_items'
+    )
     product = models.ForeignKey(
         Product,
-        on_delete=models.SET_NULL,
-        null=True,
-        blank=True,
-        related_name='stores'
+        on_delete=models.CASCADE,
+        related_name='store_items'
     )
     quantity = models.IntegerField()
-    location = models.CharField(max_length=100, blank=True)
+
+
+class Order(models.Model):
+    location = models.CharField(max_length=100)
+    price = models.DecimalField(
+        max_digits=10, decimal_places=2,
+        null=True, blank=True
+    )
+    is_paid = models.BooleanField(default=False)
+
+    def process(self):
+        store = Store.objects.get(location=self.location)
+        for item in self.order_items.all():
+            store_item = StoreItem.objects.get(
+                store=store,
+                product=item.product
+            )
+            store_item.quantity -= item.quantity
+            store_item.save()
+        self.price = sum(
+            (item.product.price * item.quantity
+                for item in self.order_items.all())
+        )
+        self.is_paid = True
+        self.save()
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(
+        Order,
+        on_delete=models.CASCADE,
+        related_name='order_items'
+    )
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name='order_items'
+    )
+    quantity = models.IntegerField()
 
 
 class Customer(models.Model):
     name = models.CharField(max_length=100)
-
-
-class Cart(models.Model):
-    customer = models.ForeignKey(
-        Customer,
-        on_delete=models.SET_NULL,
-        null=True,
-        blank=True,
-        related_name='carts'
-    )
