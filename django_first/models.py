4b28be8947c546cec9fdafd3f45fdb9d2b601286
U-NSK\Bakirova_A <Bakirova_A@PC017.nsk.kz>
2019-04-15 15:25:32 +0600

    Add exception when not enough is paid for order

--- django_first/models.py
+++ django_first/models.py
@@ -50,6 +50,10 @@ class Order(models.Model):
             (item.product.price * item.quantity
                 for item in self.order_items.all())
         )
+        confirmed_payments = self.payments.filter(is_confirmed=True)
+        paid_amount = sum((payment.amount for payment in confirmed_payments))
+        if paid_amount < self.price:
+            raise Exception('Not enough money')
         self.is_paid = True
         self.save()
 
@@ -66,6 +70,18 @@ class OrderItem(models.Model):
         related_name='order_items'
     )
     quantity = models.IntegerField()
+
+
+class Payment(models.Model):
+    order = models.ForeignKey(
+        Order, on_delete=models.CASCADE,
+        related_name='payments'
+    )
+    amount = models.DecimalField(
+        max_digits=10, decimal_places=2,
+        null=True, blank=True
+    )
+    is_confirmed = models.BooleanField(default=False)
 
 
 class Customer(models.Model):
