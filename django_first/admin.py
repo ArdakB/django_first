67e16ac612865c4b647db437eec2fb343463aa07
U-NSK\Bakirova_A <Bakirova_A@PC017.nsk.kz>
2019-04-19 09:35:19 +0600

    Changed list_display in Order and Store

--- django_first/admin.py
+++ django_first/admin.py
@@ -13,9 +13,13 @@ class StoreItemInline(admin.TabularInline):
 
 
 class StoreAdmin(admin.ModelAdmin):
-    list_display = ('id', 'location')
+    list_display = ('id', 'get_location_name')
     inlines = (StoreItemInline,)
 
+    def get_location_name(self, obj):
+        return obj.location.city.name, obj.location.address
+    get_location_name.short_description = 'Location' #Renames column head
+    get_location_name.admin_order_field = 'location__city' #Allows column order sorting
 
 class OrderItemInline(admin.StackedInline):
     model = OrderItem
@@ -23,8 +27,13 @@ class OrderItemInline(admin.StackedInline):
 
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('id', 'city')
+    list_display = ('id', 'get_city_name')
     inlines = (OrderItemInline,)
+
+    def get_city_name(self, obj):
+        return obj.city.name
+    get_city_name.short_description = 'City' #Renames column head
+    get_city_name.admin_order_field = 'city' #Allows column order sorting
 
 
 admin.site.register(Product, ProductAdmin)
