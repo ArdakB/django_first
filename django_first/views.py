7a3d65ebef30131ef8e064b11d3cd1efd3caa408
U-NSK\Bakirova_A <Bakirova_A@PC017.nsk.kz>
2019-04-28 15:33:06 +0600

    Require proper product id

--- django_first/views.py
+++ django_first/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Order, OrderItem
+from .models import Order, OrderItem, Product
 
 
 def hello(request):
@@ -14,6 +14,20 @@ def order(request, order_id):
     order = Order.objects.get(id=order_id)
     if request.method == 'POST':
         product_id = request.POST.get('product')
+        try:
+            product_id = int(product_id)
+        except ValueError:
+            return HttpResponse(
+                'Invalid product id',
+                status=400
+            )
+        try:
+            product = Product.objects.get(id=product_id)
+        except Product.DoesNotExist:
+            return HttpResponse(
+                'Product not found',
+                status=404
+            )
         quantity = request.POST.get('quantity')
         try:
             quantity = int(quantity)
@@ -29,7 +43,7 @@ def order(request, order_id):
             )
         OrderItem.objects.create(
             order=order,
-            product_id=product_id,
+            product=product,
             quantity=int(quantity)
         )
     return render(request, 'order.html', context={
